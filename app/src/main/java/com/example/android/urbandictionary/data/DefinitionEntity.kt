b314c4d4089faec679d82a0831e025ca8748733f
The doer <jesuislinn@gmail.com>
2020-04-27 12:14:17 -0500

    Adding two data classes for definition items and definition item lists

---
+++ app/src/main/java/com/example/android/urbandictionary/data/DefinitionEntity.kt
@@ -0,0 +1,4 @@
+package com.example.android.urbandictionary.data
+
+class DefinitionEntity {
+}
--- app/src/main/java/com/example/android/urbandictionary/data/DefinitionItem.kt
+++ app/src/main/java/com/example/android/urbandictionary/data/DefinitionItem.kt
@@ -1,4 +1,24 @@
 package com.example.android.urbandictionary.data
 
-class DefinitionItem {
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class DefinitionList(
+    val list: List<DefinitionItem>
+) : Parcelable
+
+@Parcelize
+data class DefinitionItem(
+    val definition: String,
+    val permalink: String,
+    val thumbs_up: String = "0",
+    val sound_urls: List<String>,
+    val author: String,
+    val word: String,
+    val defid: Int,
+    val current_vote: String,
+    val written_on: String,
+    val example: String,
+    val thumbs_down: String = "0"
+) : Parcelable
