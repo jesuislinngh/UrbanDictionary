0a88b513cefdcca839e998b28f4da9d657206315
The doer <jesuislinn@gmail.com>
2020-04-28 06:39:58 -0500

    Passing all the object's definition to a single file/class

--- app/src/main/java/com/example/android/urbandictionary/data/DefinitionEntity.kt
+++ app/src/main/java/com/example/android/urbandictionary/data/DefinitionEntity.kt
@@ -1,4 +1,26 @@
 package com.example.android.urbandictionary.data
 
-class DefinitionEntity {
-}
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class DefinitionList(
+    val list: List<DefinitionItem>
+) : Parcelable
+
+@Parcelize
+@Entity(tableName = "definitions_table")
+data class DefinitionItem (@PrimaryKey @ColumnInfo(name = "word") val word: String,
+                             val definition: String,
+                             val permalink: String,
+                             val thumbs_up: String = "0",
+                             val sound_urls: List<String>,
+                             val author: String,
+                             val defid: Int,
+                             val current_vote: String,
+                             val written_on: String,
+                             val example: String,
+                             val thumbs_down: String = "0") : Parcelable
---
+++ app/src/main/java/com/example/android/urbandictionary/results/AdapterDefinitionResults.kt
@@ -0,0 +1,4 @@
+package com.example.android.urbandictionary.results
+
+class AdapterDefinitionResults {
+}
