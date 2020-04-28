318a97717e3f62308c0848de09c274bf1b85e135
The doer <jesuislinn@gmail.com>
2020-04-28 06:43:13 -0500

    Adding room definitions

--- app/src/main/java/com/example/android/urbandictionary/data/DefinitionItemDao.kt
+++ app/src/main/java/com/example/android/urbandictionary/data/DefinitionItemDao.kt
@@ -1,4 +1,19 @@
 package com.example.android.urbandictionary.data
 
-class DefinitionItemDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DefinitionItemDao {
+    @Query("Select * from definitions_table ORDER BY word ASC")
+    fun getDefinitions(): LiveData<List<DefinitionItem>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(definitionItem: DefinitionItem)
+
+    @Query("DELETE FROM definitions_table")
+    suspend fun deleteAll()
 }
