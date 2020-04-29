35ba87189019e96fb0f8000b5e42a5ae5fe48767
The doer <jesuislinn@gmail.com>
2020-04-29 14:34:21 -0500

    Starting to create the empty estructure

--- app/src/main/java/com/example/android/urbandictionary/results/ViewModelResults.kt
+++ app/src/main/java/com/example/android/urbandictionary/results/ViewModelResults.kt
@@ -1,7 +1,14 @@
 package com.example.android.urbandictionary.results
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.urbandictionary.data.DefinitionItem
 
 class ViewModelResults : ViewModel() {
     // TODO: Implement the ViewModel
+    private val _definitions = MutableLiveData<List<DefinitionItem>>()
+
+    val definitions: LiveData<List<DefinitionItem>>
+        get() = _definitions
 }
