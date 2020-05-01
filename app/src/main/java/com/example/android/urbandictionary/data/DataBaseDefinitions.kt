ae6b44d538d50f1e062e5cbdc7910c27287994aa
The doer <jesuislinn@gmail.com>
2020-05-01 14:36:00 -0500

    Database definition

--- app/src/main/java/com/example/android/urbandictionary/data/DataBaseDefinitions.kt
+++ app/src/main/java/com/example/android/urbandictionary/data/DataBaseDefinitions.kt
@@ -1,4 +1,54 @@
 package com.example.android.urbandictionary.data
 
-class DataBaseDefinitions {
+import android.content.Context
+import android.util.Log
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+
+private const val  DATABASE_NAME = "urbandictionary_database"
+
+@Database(entities = [DefinitionEntity::class], version = 1, exportSchema = false)
+abstract class DataBaseDefinitions : RoomDatabase() {
+
+    abstract fun daoDefinition(): DaoDefinitionItem
+
+    companion object {
+
+        private val TAG =  DataBaseDefinitions::class.java.canonicalName
+
+        @Volatile
+        private var INSTANCE: DataBaseDefinitions? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): DataBaseDefinitions {
+
+            if (INSTANCE != null) return INSTANCE as DataBaseDefinitions
+
+            synchronized(this) {
+                return Room.databaseBuilder(
+                    context.applicationContext,
+                    DataBaseDefinitions::class.java,
+                    DATABASE_NAME
+                ).addCallback(WordDatabaseCallback(scope)).build()
+            }
+        }
+
+    }
+
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+
+            INSTANCE?.let { database ->
+                scope.launch {
+                    Log.d(TAG, "$DATABASE_NAME created")
+                }
+            }
+        }
+    }
 }
