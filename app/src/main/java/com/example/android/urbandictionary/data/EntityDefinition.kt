01c3eeee92a854b69ff2a3fded879cab954bb2bc
The doer <jesuislinn@gmail.com>
2020-05-05 20:13:31 -0500

    DB table cannot save list objects in fields, changed to string, will look into this later

--- app/src/main/java/com/example/android/urbandictionary/data/EntityDefinition.kt
+++ app/src/main/java/com/example/android/urbandictionary/data/EntityDefinition.kt
@@ -7,18 +7,32 @@ import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 
+@Parcelize
 @Entity(tableName = "definitions_table")
 data class DefinitionEntity (@PrimaryKey @ColumnInfo(name = "word") val word: String,
                              val definition: String,
                              val permalink: String,
                              val thumbs_up: String = "0",
-                             val sound_urls: List<String>,
+                             val sound_urls: String,
                              val author: String,
                              val defid: Int,
                              val current_vote: String,
                              val written_on: String,
                              val example: String,
-                             val thumbs_down: String = "0")
+                             val thumbs_down: String = "0") : Parcelable
+
+fun DefinitionEntity.toDefinitionItem() = DefinitionItem(
+    word = word,
+    definition = definition,
+    permalink = permalink,
+    thumbs_up = thumbs_up,
+    sound_urls = listOf(sound_urls),
+    author = author,
+    defid = defid,
+    current_vote = current_vote,
+    written_on = written_on,
+    example = example,
+    thumbs_down = thumbs_down)
 
 @Parcelize
 data class DefinitionList(
