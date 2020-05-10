9dca367c9dafaf7ae0989a36821cc9d463202e0b
The doer <jesuislinn@gmail.com>
2020-05-10 18:02:14 -0500

    Minor update on merge, really no changes made

--- app/src/main/java/com/example/android/urbandictionary/search/FragmentSearch.kt
+++ app/src/main/java/com/example/android/urbandictionary/search/FragmentSearch.kt
@@ -26,7 +26,6 @@ class FragmentSearch : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
     }
 
     override fun onCreateView(
